4e7a595debf059e1408403ac1f33fad0a9ae1467
ucac1912032 <ucac1912032@gmail.com>
2024-10-08 15:26:10 +0900

    Update project.py

--- project.py
+++ project.py
@@ -1,44 +1,47 @@
 import streamlit as st
 import pandas as pd
 
-st.title("📦재고관리 앱📦")
+st.title('재고관리 앱')
 
 # 초기 데이터프레임
-df = pd.DataFrame(
-    [
-        {"부품명": "", "개수": 0, "배송여부": False}
-    ]
-)
+if 'data' not in st.session_state:
+    st.session_state.data = pd.DataFrame(
+        [
+            {"부품명": "", "개수": 0, "배송여부": False},
+            {"부품명": "", "개수": 0, "배송여부": False},
+            {"부품명": "", "개수": 0, "배송여부": False},
+        ]
+    )
 
 # CSV 파일 업로드 기능
 uploaded_file = st.file_uploader("CSV 파일 업로드", type='csv')
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+    st.session_state.data = pd.read_csv(uploaded_file)
 
 # 데이터 수정 가능한 에디터
-edited_df = st.data_editor(df)
+edited_df = st.data_editor(st.session_state.data)
 
 # 부품 추가 기능
 st.subheader("부품 추가")
-부품명 = st.text_input("부품명")
-개수 = st.number_input("개수", min_value=0)
-배송여부 = st.checkbox("배송여부")
+부품명 = st.text_input("부품명", key='부품명')
+개수 = st.number_input("개수", min_value=0, key='개수')
+배송여부 = st.checkbox("배송여부", key='배송여부')
 
 if st.button("부품 추가"):
     new_row = pd.DataFrame({"부품명": [부품명], "개수": [개수], "배송여부": [배송여부]})
-    edited_df = pd.concat([edited_df, new_row], ignore_index=True)
+    st.session_state.data = pd.concat([st.session_state.data, new_row], ignore_index=True)
     st.success("부품이 추가되었습니다!")
 
 # 최종 수정된 데이터프레임 표시
 st.subheader("수정된 재고 리스트")
-st.dataframe(edited_df)
+st.dataframe(st.session_state.data)
 
 # 사용자로부터 파일명 입력받기
 file_name = st.text_input("저장할 파일명", "재고리스트.csv")
 
 # 수정된 데이터프레임을 CSV로 다운로드
-if st.button("CSV로 다운로드"):
-    csv = edited_df.to_csv(index=False).encode('utf-8')
+if st.button("다운로드"):
+    csv = st.session_state.data.to_csv(index=False).encode('utf-8')
     st.download_button(
         label="CSV 다운로드",
         data=csv,
